439309b93616c09330d79544832aa3cec0c536dc
MrCasuela <pablosepu004@gmail.com>
2025-05-25 19:13:12 -0400

    Inicio del Proyecto

---
+++ src/cl/unab/inf/tarea2/frontend/PanelPrincipal.java
@@ -0,0 +1,11 @@
+package cl.unab.inf.tarea2.frontend;
+
+import javax.swing.*;
+
+public class PanelPrincipal {
+    private JPanel contenedor;
+    private JPanel botonera;
+    private JButton cargarArchivo;
+    private JLabel textoConfirmador;
+    private JLabel titulo;
+}
